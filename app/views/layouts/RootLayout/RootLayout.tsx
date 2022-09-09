1bcf885d6f3f4dcda8eac84463dc3fb47190babf
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-09 19:10:43 -0400

    321123

--- app/views/layouts/RootLayout/RootLayout.tsx
+++ app/views/layouts/RootLayout/RootLayout.tsx
@@ -1,15 +1,17 @@
 
 import type { ILayout } from "@models/typings/Layout"
-
+import CssBaseLine from "@mui/material/CssBaseLine"
+import Box from "@mui/material/Box"
 import Meta from "@includes/Meta"
 
 const RootLayout = ({ children, metaData }: ILayout) => {
 
     return (
-        <>
+        <Box>
             <Meta {...metaData} />
+            <CssBaseLine />
             {children}
-        </>
+        </Box>
     )
 }
 
