f481774705201c5ecd3072fd2323a92640e5a1f5
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-09 23:19:09 -0400

    dw

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,15 +3,22 @@ import LogoRow from "@components/LogoRow"
 import homePage from '@pages/homePage'
 import PageLayout from '@views/layouts/PageLayout'
 import Hero from "@components/Hero"
-
-
+import Gallery from "@components/Gallery"
+import MediaRow from "@views/components/MediaRow"
+import Summary from "@views/components/Summary"
 const HomePage = ({ pageData }) => {
 
   return (
     <PageLayout {...pageData.layout}>
       <Hero {...pageData.hero} />
-      <Featured {...pageData.featured} />
       <LogoRow />
+
+      <Featured {...pageData.featured} />
+      <MediaRow />
+
+      <Summary />
+
+      <Gallery />
     </PageLayout>
   )
 }
