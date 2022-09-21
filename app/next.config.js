20eff739af118ce88ebbf5356a8c2bd39caafc93
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-21 15:16:33 -0400

    dwdw

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  env: {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+  },
 }
 
 module.exports = nextConfig
