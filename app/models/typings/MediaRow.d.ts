1f620ee4fa60ce6b1e19cb430defa9ea1321bd54
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-25 16:16:01 -0400

    [ corrected type ]

--- app/models/typings/MediaRow.d.ts
+++ app/models/typings/MediaRow.d.ts
@@ -2,6 +2,8 @@ import type { ImageProps } from "@models/typings/Image"
 
 export type MediaProps = {
     key?: any,
+    url?: string,
+    version?: string,
     cover?: ImageProps,
     title?: string,
     description?: string
