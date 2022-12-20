c6c9889189df2b4c714b70916d36ab3b3c36c1cd
Jeffrey J. Desir <devops@informandinspire.llc>
2022-12-20 06:14:59 -0500

    [...]

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,52 +1,41 @@
 import "@libs/animations.css";
+import "@libs/dashvars.css";
 import "@libs/globals.css";
+import "@libs/reset.css";
 import "@libs/scrollbar.css";
 import "@libs/tailwind.css";
-import "@libs/reset.css";
-import "@libs/dashvars.css";
 
-import PageLayout from "@layouts/PageLayout";
-
-
-import PageService from "@controllers/services/pages";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-
 import { PageTransition } from 'next-page-transitions';
 
 
+function Application({ Component, pageProps }: AppProps & any) {
 
-export type ApplicationProps = AppProps & {
-  layout?: any
-}
-
-function Application({ Component, pageProps, layout }: ApplicationProps) {
-
-
-  const { getLayout } = Component as any
 
   return (
     Component ?
-      <PageTransition loadingDelay={200} loadingComponent={<>Loading Application</>} timeout={200} classNames={"page-transition"}>
-        <RecoilRoot>
-          <PageLayout {...layout}>
-            <Component {...pageProps} />
-          </PageLayout>
-        </RecoilRoot>
-      </PageTransition> : <>Loading Application</>
-
-
+      <RecoilRoot>
+        {
+          Component?.layout ?
+            <Component.layout {...pageProps.page.layout}>
+              <PageTransition loadingDelay={200} loadingComponent={<>Loading Application</>} timeout={200} classNames={"page-transition"}>
+                <Component {...pageProps} />
+              </PageTransition>
+            </Component.layout>
+            :
+            <>
+              <Component {...pageProps} />
+            </>
+        }
+      </RecoilRoot>
+      :
+      <>Loading Application</>
   )
 }
 
-
-Application.getInitialProps = async () => {
-  const { getPage } = PageService();
-  const { layout } = await getPage("home");
-  return { layout };
-};
 
 export default Application
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,32 @@
 import Featured from "@components/Featured"
 import Grid from "@components/Grid"
 import Hero from "@components/Hero"
+import LogoCloud from "@components/LogoCloud"
 import MediaRow from "@components/MediaRow"
 import Summary from "@components/Summary"
+import PageLayout from "@layouts/PageLayout"
 import PageService from "@services/pages"
-import LogoCloud from "@views/components/LogoCloud"
 
-const HomePage = ({ page: { data } }) => {
 
-  const { hero, featured, mediaRow, summary, grid, logoCloud } = data
+export async function getStaticProps() {
+
+  const { getPage } = PageService()
+
+  const page = await getPage("home")
+
+  return {
+    props: {
+      page
+    },
+    revalidate: 1
+  }
+}
+
+
+
+const HomePage = ({ page }) => {
+
+  const { hero, featured, mediaRow, summary, grid, logoCloud } = page?.data ?? null
 
   return (
     <>
@@ -22,19 +40,8 @@ const HomePage = ({ page: { data } }) => {
   )
 }
 
+HomePage.layout = PageLayout
+
 export default HomePage
 
-
-export async function getServerSideProps() {
-
-  const { getPage } = PageService()
-
-  const page = await getPage("home")
-
-  return {
-    props: {
-      page
-    },
-  }
-}
 
