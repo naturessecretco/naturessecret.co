6d2be60ecd91dd23e1b79083be2e9994ee47adb7
Jeffrey J. Desir <devops@informandinspire.llc>
2023-01-14 16:10:12 -0500

    [][][]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -7,6 +7,14 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown(), react(), sitemap()]
+  integrations: [tailwind(), partytown(), react(), sitemap(), mdx(), image(), prefetch()],
+  output: "server",
+  adapter: vercel()
 });
