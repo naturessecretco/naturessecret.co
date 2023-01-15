0366b4b9e621555b3bc8f11b33da1d499b8638dd
Jeffrey J. Desir <devops@informandinspire.llc>
2023-01-14 19:51:36 -0500

    [][][]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), react()]
 });
