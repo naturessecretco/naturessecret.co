06d3cd24150b2cc18078f3b0ed6a029b2e7378fd
Jeffrey J. Desir <devops@informandinspire.llc>
2023-01-24 14:23:18 -0500

    [,.]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -8,6 +8,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://naturessecret.co",
   integrations: [tailwind(), svelte(), solidJs()],
   output: "server",
   adapter: vercel()
