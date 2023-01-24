4e93309038cfdcf0d18ab5459d23c98f4de95fff
Jeffrey J. Desir <devops@informandinspire.llc>
2023-01-24 15:51:28 -0500

    [ Boom Shakalaka ]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()],
   output: "server",
   adapter: vercel()
 });
