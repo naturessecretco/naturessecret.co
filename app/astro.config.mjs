e5ccc2b6c72fd442ed7bc37c9579a47b001f4099
Jeffrey J. Desir <devops@informandinspire.llc>
2023-01-24 15:28:24 -0500

    [ Last Ditch Effort ]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,15 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
-export default defineConfig({
-  site: "https://naturessecret.co",
-  integrations: [tailwind(), svelte(), solidJs()],
-  output: "static",
-  adapter: vercel()
-});
+export default defineConfig({});
