59c7b77485782fe5e9617a84f54b2286019e5470
Jeffrey J. Desir <devops@informandinspire.llc>
2023-02-04 13:25:04 -0500

    [ Ayyeeee ]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -16,5 +16,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch(), react()],
-  output: "static"
+  output: "server",
+  adapter: vercel()
 });
